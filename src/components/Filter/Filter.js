cdcb1d4ad7b683483fe457e6c63e66c493cd3e01
Alex Mandebura <xenovidbis@gmail.com>
2023-01-28 23:40:56 +0200

    fix bugs

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,24 +1,20 @@
+import css from './Filter.module.css';
 import PropTypes from 'prop-types';
-import s from './Filter.module.css';
-
-export default function Filter({ filter, handleChange }) {
-    return (
-      <label className={s.label}>
-        Find contacts by name
-        <input
-          className={s.input}
-          type="text"
-          name="filter"
-          value={filter}
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          onChange={handleChange}
-          required
-        />
-      </label>
-    );
-}
-
-Filter.prototype = {
-    filter: PropTypes.string.isRequired,
-    handleChange: PropTypes.func.isRequired,
+export const Filter = ({ value, onChange }) => {
+  return (
+    <label className={css.Filter__label} htmlFor="filter">
+      Find contacts by name
+      <input
+        className={css.Filter__input}
+        type="text"
+        name="filter"
+        value={value}
+        onChange={onChange}
+      />
+    </label>
+  );
+};
+Filter.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
 };
